a6ffe8884953ac8d8d72ed53644b24feda58d329
willmangione <mangionw@bc.edu>
2015-08-06 16:16:15 -0400

    finished pack and str

--- src/PolyPacker.py
+++ src/PolyPacker.py
@@ -1,6 +1,6 @@
 
 
-from LinePacker import LinePacker, Unpackable
+from LinePacker import Unpackable
 from LinePacker2D import LinePacker2D
 
 class PolyPacker(object):
@@ -19,23 +19,35 @@ class PolyPacker(object):
         return any(lp2D.canfit(line_len) for lp2D in self.lines)
 
     def pack(self, line_len, near=0+0j):
-
+        if not self.canfit(line_len):
+            raise Unpackable
         dists = []
         i = 0
         for lp2D in self.lines:
             try:
-                pack = lp2D.pack(line_len, near)
+                pack = lp2D.pack(line_len, near, check=True)
             except Unpackable:
                 i = i + 1
-                pass
             else:
                 dists.append((pack[1],i))
                 i = i + 1
         min_index = sorted(dists, key=lambda x: x[0])[0][1]
         #the index in the self.lines list that has the desired LP2D object
-        pass
+        line, dist = self.lines[min_index].pack(line_len, near)
+        return line, dist
+
+    def __str__(self):
+        pack_str = ""
+        for line in self.lines:
+            skeleton = "From %s " % str(line.orig_line.point(0.0)) + \
+                       "to %s: " % str(line.orig_line.point(1.0))
+            pack_str = pack_str + skeleton + str(line) + "| "
+        return pack_str
 
 
-
-poly = PolyPacker([0+0j, 0+10j, 10+10j, 10+0j])
-print poly.pack(4, 3+8j)
+if __name__ == "__main__":
+    poly = PolyPacker([0+0j, 0+10j, 10+10j, 10+0j])
+    print poly.pack(4, 9+8j)
+    print poly.pack(4, 9+8j)
+    print poly.pack(4, 9+8j)
+    print poly
