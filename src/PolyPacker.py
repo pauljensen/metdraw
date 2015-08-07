f75cd0e47432da62069aaeed999dd54f7f4b4614
Paul Jensen <paul.jensen@bc.edu>
2015-08-06 23:26:47 -0500

    formatting changes

--- src/PolyPacker.py
+++ src/PolyPacker.py
@@ -3,17 +3,17 @@
 from LinePacker import Unpackable
 from LinePacker2D import LinePacker2D
 
-class PolyPacker(object):
 
+class PolyPacker(object):
     def __init__(self, points):
         self.points = points
         self.n_points = len(points)
         self.lines = []
         for i in range(0, self.n_points-1):
-            self.lines.append(LinePacker2D(
-                self.points[i], self.points[i+1]))
-        self.lines.append(LinePacker2D(
-            self.points[self.n_points-1], self.points[0]))
+            self.lines.append(LinePacker2D(self.points[i],
+                                           self.points[i+1]))
+        self.lines.append(LinePacker2D(self.points[self.n_points-1],
+                                       self.points[0]))
 
     def canfit(self, line_len):
         return any(lp2D.canfit(line_len) for lp2D in self.lines)
@@ -22,25 +22,23 @@ class PolyPacker(object):
         if not self.canfit(line_len):
             raise Unpackable
         dists = []
-        i = 0
-        for lp2D in self.lines:
+        for i, lp2D in enumerate(self.lines):
             try:
                 pack = lp2D.pack(line_len, near, check=True)
             except Unpackable:
-                i = i + 1
+                pass
             else:
-                dists.append((pack[1],i))
-                i = i + 1
+                dists.append((pack[1], i))
         min_index = sorted(dists, key=lambda x: x[0])[0][1]
-        #the index in the self.lines list that has the desired LP2D object
+        # the index in the self.lines list that has the desired LP2D object
         line, dist = self.lines[min_index].pack(line_len, near)
         return line, dist
 
     def __str__(self):
         pack_str = ""
         for line in self.lines:
-            skeleton = "From %s " % str(line.orig_line.point(0.0)) + \
-                       "to %s: " % str(line.orig_line.point(1.0))
+            skeleton = ("From %s " % str(line.orig_line.point(0.0)) +
+                        "to %s: " % str(line.orig_line.point(1.0)))
             pack_str = pack_str + skeleton + str(line) + "| "
         return pack_str
 
