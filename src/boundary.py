b133c9ac92b512286c58e6d782a60d549a3aa9f8
willmangione <mangionw@bc.edu>
2015-11-19 11:31:13 -0500

    converting to metdraw compatability

--- src/boundary.py
+++ src/boundary.py
@@ -4,18 +4,7 @@ from svg.path import Line, parse_path, Path
 from packing import PolyPacker
 from exchange_layout import gen_graph
 from rxns import *
-
-
-def tagmatch(elem,tag):
-    return elem.tag.endswith('}' + tag)
-
-def findfirst(elem,tag):
-    for e in elem.iter():
-        if tagmatch(e,tag):
-            return e
-
-def findall(elem,tag):
-    return [e for e in elem.iter() if tagmatch(e,tag)]
+from sbml import findfirst, findall
 
 tag = 'ns0:'
 
@@ -27,7 +16,7 @@ def remove_pt(numb):
     new_numb = numb.replace("pt", '')
     return float(new_numb)
 
-#finds the size of the reaction from the halfway point of the midpoint segment
+#finds the size of the reaction from the halfway point of the  segment
 #  to the major/minor product arrows (currently not in use)
 def rxn_size(svg_tree):
     mp = ""
@@ -60,6 +49,16 @@ def get_box_points(box_path):
     point2 = path1.point(y_ratio)
     point4 = path1.point(0.5 + y_ratio)
     return [point1, point2, point3, point4]
+#updated version compatible with metdraw svg output
+def get_box_points_new(compartment_element):
+    ptList = []
+    raw_list = findfirst(compartment_element, 'polygon').get('points').replace(' ',',').split(',')
+    for i in xrange(0,8,2):
+        pt = gen_point(float(raw_list[i]),float(raw_list[i+1]))
+        ptList.append(pt)
+    fourth = ptList.pop(3)
+    ptList.insert(2, fourth)
+    return ptList
 
 #generates the exchange membrane path given a list of box points
 def gen_membrane_path(boxPts):
@@ -138,29 +137,55 @@ def metab_dict(glist):
             dct[name] = gen_point(x,y)
     return dct
 
-file2 = open("box.svg", "r")
+file2 = open("small_rxn.svg", "r")
 file3 = open("output.svg", "w")
 tree2 = ElementTree(file=file2)
 graph2 = tree2.getroot()
 glist2 = findall(graph2, 'g')
+for poly in glist2:
+    if poly.get('class') == 'cluster':
+        comp = findfirst(poly, 'text').text
+        if comp == 'Extracellular':
+            compPts = get_box_points_new(poly)
+        if comp == 'Periplasm':
+            compPts = get_box_points_new(poly)
+        if comp == 'Cytoplasm':
+            compPts = get_box_points_new(poly)
+print compPts
+
 
 #create the new membrane box path
-boxPath = findfirst(graph2, 'path').get('d')
-boxStart = parse_path(boxPath)
-boxPoints = get_box_points(boxPath)
-newBoxPath = gen_membrane_path(boxPoints)
+#boxPath = findfirst(graph2, 'path').get('d')
+#print boxPath
+#boxStart = parse_path(boxPath)
+#boxPoints = get_box_points(boxPath)
+#print boxPoints
+#newBoxPath = gen_membrane_path(boxPoints)
+#newBoxPts = get_box_points(newBoxPath)
+#memBoxStart = parse_path(newBoxPath)
+#pathElement = Element(tag + 'path',
+                      #attrib={'d': '' + gen_membrane_path(boxPoints)})
+newBoxPath = gen_membrane_path(compPts)
 newBoxPts = get_box_points(newBoxPath)
+print newBoxPts
 memBoxStart = parse_path(newBoxPath)
+newPath = gen_membrane_path(newBoxPts)
+newBoxElement = Element(tag + 'g', attrib={'style': "stroke: black; fill: none;"})
 pathElement = Element(tag + 'path',
-                      attrib={'d': '' + gen_membrane_path(boxPoints)})
-boxList = findall(graph2[0], 'g')
-for g in boxList:
-    if g.get('style') != None:
-        g.append(pathElement) #writes the new box path in the file
+                      attrib={'d': '' + newPath})
+print newPath
+newBoxElement.append(pathElement)
+#boxList = findall(graph2[0], 'g')
+#for g in boxList:
+    #if g.get('style') != None:
+        #g.append(pathElement)
+
+#writes the new box path in the file
+graph2.append(newBoxElement)
 
 #create PolyPacker object
 pp = PolyPacker(newBoxPts)
-line_len = Line(newBoxPts[0], boxPoints[0]).length()
+line_len = Line(newBoxPts[0], compPts[0]).length()
 pp.pack(line_len, near=newBoxPts[0])
 pp.pack(line_len, near=newBoxPts[1])
 pp.pack(line_len, near=newBoxPts[2])
@@ -178,8 +203,8 @@ metabDict = metab_dict(graph2)
 #rxnList is just a list of reactions to place
 for rxn in rxnList:
     graphDot = gen_graph(rxn)
-    graphDot.export_graphviz(output="svg", filename="dotprac2")
-    file1 = open("dotprac2.svg", "r")
+    graphDot.export_graphviz(output="svg", filename="small_rxn_ex")
+    file1 = open("small_rxn_ex.svg", "r")
     tree = ElementTree(file=file1)
     graph_root = tree.getroot()
     graph1 = graph_root[0]
