45fa9be65c2f7d9e185909274f2b0071a99ef0d4
willmangione <mangionw@bc.edu>
2015-12-31 20:22:24 -0500

    added exchange rxn placement code

--- src/metdraw.py
+++ src/metdraw.py
@@ -3,6 +3,11 @@
 import model as Model
 import minors as Minors
 import sbml, layout, util, model_json, gpr
+import boundary as Bnd
+from xml.etree.ElementTree import *
+from packing import *
+import exchange_layout as Ex
+import svg.path as Path
 
 import argparse, os
 
@@ -120,6 +125,9 @@ def metdraw(filename,count_mets=None,met_file=None,show=False,
     if show:
         model.display()
         display_parameters(defaults)
+
+    SHOW_EXCHANGES = model.get_param('SHOW_EXCHANGES')
+    compartments = Model.get_comps(model.compartments)
     
     if not quiet:
         print 'Creating reaction layout'
@@ -149,6 +157,124 @@ def metdraw(filename,count_mets=None,met_file=None,show=False,
     else:
         print 'ok'
 
+    if SHOW_EXCHANGES:
+        main_file = open(output_filename, "r")
+        main_tree = ElementTree(file=main_file)
+        main_graph = main_tree.getroot()
+        main_glist = sbml.findall(main_graph, 'g')
+        comps_exs = Model.get_exchanges(model)
+        comps_pts = Bnd.get_compPts(main_glist)
+        insides = dict(Model.get_insides(model.compartments))
+        metabDict = Bnd.metab_dict(main_glist)
+        for comp in compartments:
+            rxnList = comps_exs[comp[0].id]
+            if comp[0].outside != None:
+                comp_pts = comps_pts[comp[0].id]
+            else:
+                comp_pts = comps_pts[insides[comp[0].id]]
+
+            newBoxPath = Bnd.gen_membrane_path(comp_pts)
+            memBoxStart = Bnd.parse_path(newBoxPath)
+
+            pp = PolyPacker(comp_pts)
+            line_len = Line(0+0j, 0+150j).length() #default 150 buffer
+
+            pp.pack(line_len, near=comp_pts[0])
+            pp.pack(line_len, near=comp_pts[1])
+            pp.pack(line_len, near=comp_pts[2])
+            pp.pack(line_len, near=comp_pts[3])
+
+            leftLine = Line(comp_pts[0], comp_pts[1]) #creates the line
+            bottomLine = Line(comp_pts[1], comp_pts[2])
+            #rightLine = Line(comp_pts[2], comp_pts[3])
+            topLine = Line(comp_pts[3], comp_pts[0])
+
+            top_mid = topLine.point(.5)
+            top_bott = bottomLine.point(.5)
+            pp.pack(700.0, near=top_mid) #to avoid packing over compartment label
+
+
+
+
+            memBoxStart_x = memBoxStart.point(0).real
+            memBoxStart_y = memBoxStart.point(0).imag
+
+            for rxn in rxnList:
+                if (len(rxn.reactants) + len(rxn.products)) > 20:
+                    continue #to avoid placing biomass reactions
+                graphDot = Ex.gen_graph(rxn)
+                graphDot.export_graphviz(output="svg", filename="ex", warnings=False)
+                ex_file = open("ex.svg", "r")
+                ex_tree = ElementTree(file=ex_file)
+                graph_root = ex_tree.getroot()
+                ex_graph = graph_root[0]
+                width = Bnd.remove_pt(sbml.findfirst(ex_tree, 'svg').get('width'))
+                ex_glist = sbml.findall(ex_graph, 'g')
+                #nodeList = Bnd.get_nodes(ex_glist)
+                edgeList = Bnd.get_edges(ex_glist)
+
+                for edge in edgeList:
+                    path = sbml.findfirst(edge, 'path')
+                    path.set('d', Bnd.extend_edge(path).get('d')) #smooth out edges
+                    if edge.get('id') == "midpoint":
+                        path = sbml.findfirst(edge, 'path')
+                        d = path.get('d')
+                        x = Path.parse_path(d).point(0.0).real
+                        path_end = Path.parse_path(d).point(1.0)
+                        midpoint = Path.parse_path(d).point(.5)
+                path_x = path_end.real
+
+                g = sbml.findfirst(ex_graph, 'polygon') #this solves the overlapping issue where
+                                     # the box was covering the rxns
+                g.set('fill', 'none')
+
+                scale = "scale(1 1) "
+                metabList = rxn.major_products
+                if Bnd.metab_overlap(metabList, metabDict):
+                    name = Bnd.common_metab(metabList, metabDict)
+                    near = metabDict[name]
+                else:
+                    near = Bnd.gen_point(bottomLine.point(.5).real, leftLine.point(.5).imag)
+                pack = pp.pack(width + 8.0, near=near)
+                pack_mp = pack[0].point(.5)
+                if pack[2] == 3:
+                    rotateTop = "rotate(%f %f %f) " % (0, memBoxStart_x, memBoxStart_y)
+                    translateTop = "translate(%f %f)" % (pack_mp.real - path_x,
+                                                         memBoxStart_y - midpoint.imag)
+                    transform = scale + rotateTop + translateTop
+                if pack[2] == 0:
+                    rotateLeft = "rotate(%f %f %f) " % (270, memBoxStart_x, memBoxStart_y)
+                    translateLeft = "translate(%f %f)" % (memBoxStart_x + memBoxStart_y -
+                                                          path_x - pack_mp.imag,
+                                                          memBoxStart_y - midpoint.imag)
+                    transform = scale + rotateLeft + translateLeft
+                if pack[2] == 1:
+                    rotateBott = "rotate(%f %f %f) " % (180, memBoxStart_x, memBoxStart_y)
+                    translateBott = "translate(%f %f)" % (memBoxStart_x + memBoxStart_x
+                                                            - path_x - pack_mp.real,
+                                                            memBoxStart_y - midpoint.imag
+                                                            - leftLine.length())
+                    transform = scale + rotateBott + translateBott
+                if pack[2] == 2:
+                    rotateRight = "rotate(%f %f %f) " % (90, memBoxStart_x, memBoxStart_y)
+                    translateRight = "translate(%f %f)" % (memBoxStart_x - path_x +
+                                                           pack_mp.imag - memBoxStart_y,
+                                                           memBoxStart_y - midpoint.imag -
+                                                           topLine.length())
+                    transform = scale + rotateRight + translateRight
+
+                for g in ex_glist:
+                    if g.get('id') == 'rxn':
+                        g.set('transform', transform)
+
+                main_graph[0].append(ex_graph)
+                os.remove('ex.svg')
+            main_tree.write(output_filename)
+
+    #trans = sbml.findfirst(main_graph, 'g').get('transform')
+    #trans = Bnd.trans_parse(trans)
+    #tf = 'transform="scale(1 1) rotate(0) translate(%f %f)"' % (trans[0], trans[1])
+    #sbml.findfirst(main_tree, 'g').set('transform', tf)
     # clean up intermediate DOT file
     os.remove(dot_filename)
 
@@ -204,3 +330,6 @@ if __name__ == '__main__':
         update_defaults(params)
 
     metdraw(**metdraw_args)
+
+
+
